0e7117c22eeaa13957f769862b4f23f06c4170e9
adilmarghadi1 <adilmarghadi2020@gmail.com>
2022-03-22 10:09:40 +0100

    first

---
+++ app.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
+const mongoose = require('mongoose');
+const passport = require('passport');
+const flash = require('connect-flash');
+const session = require('express-session');
+require('dotenv').config()
+
+const app = express();
+
+// Passport Config
+require('./config/passport')(passport);
+
+mongoose.connect(process.env.MONGO_URI,
+  err => {
+      if(err) throw err
+
+      console.log("Connected Successfully to MongoDB")
+  });
+
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
+ 
+
+// Express session
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true,
+    // cookie: { maxAge: 60 * 60 * 1000 } 
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Connect flash
+app.use(flash());
+
+// Global variables
+app.use(function(req, res, next) {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
+
+// Routes
+app.use('/', require('./routes/index.js'));
+app.use('/users', require('./routes/users.js'));
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server running on  ${PORT}`));
